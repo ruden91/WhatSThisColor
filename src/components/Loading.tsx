457269d0ccfc0f94126d6336ab983acdc628c070
ruden <ruden91@naver.com>
2018-03-23 19:41:01 +0900

    loading component 스타일 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
 
-const Loading: React.SFC = () => <div>Loading...</div>;
+const Loading: React.SFC = () => (
+  <div className="loader-overlay">
+    <div className="loader">
+      <div className="dot accent-orange" />
+      <div className="dot accent-pink" />
+      <div className="dot accent-green" />
+      <div className="dot accent-cyan" />
+      <div className="dot accent-blue" />
+      <div className="dot accent-purple" />
+    </div>
+  </div>
+);
 
 export default Loading;
